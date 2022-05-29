720abcd797b2a06f1e347ed6c473cb112db51175
lmx <1721261216@qq.com>
2022-05-29 14:27:18 +0800

    feature: project1

---
+++ kv/storage/standalone_storage/badger_reader.go
@@ -0,0 +1,41 @@
+package standalone_storage
+
+import (
+	"errors"
+	"github.com/Connor1996/badger"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
+)
+
+type BadgerReader struct {
+	db  *badger.DB
+	Txn *badger.Txn
+}
+
+func NewBadgerReader(db *badger.DB) *BadgerReader {
+	txn := db.NewTransaction(false)
+	return &BadgerReader{
+		db:  db,
+		Txn: txn,
+	}
+}
+
+func (b *BadgerReader) GetCF(cf string, key []byte) ([]byte, error) {
+	data, err := engine_util.GetCF(b.db, cf, key)
+	if err != nil {
+		if errors.Is(err, badger.ErrKeyNotFound) {
+			// it is not error
+			return nil, nil
+		}
+		return nil, err
+	}
+	return data, err
+}
+
+func (b *BadgerReader) IterCF(cf string) engine_util.DBIterator {
+	return engine_util.NewCFIterator(cf, b.Txn)
+}
+
+func (b *BadgerReader) Close() {
+	b.db = nil
+	b.Txn.Discard()
+}
--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,8 +1,10 @@
 package standalone_storage
 
 import (
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -10,11 +12,15 @@ import (
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	db *badger.DB
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	db := engine_util.CreateDB(conf.DBPath, false)
+	return &StandAloneStorage{
+		db: db,
+	}
 }
 
 func (s *StandAloneStorage) Start() error {
@@ -24,15 +30,32 @@ func (s *StandAloneStorage) Start() error {
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
-	return nil
+	err := s.db.Close()
+	return err
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return nil, nil
+	reader := NewBadgerReader(s.db)
+	return reader, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
-	// Your Code Here (1).
+	// Your Code Here (1)
+	txn := s.db.NewTransaction(true)
+	for _, v := range batch {
+		switch v.Data.(type) {
+		case storage.Put:
+			put := v.Data.(storage.Put)
+			txn.Set(engine_util.KeyWithCF(put.Cf, put.Key), put.Value)
+		case storage.Delete:
+			del := v.Data.(storage.Delete)
+			txn.Delete(engine_util.KeyWithCF(del.Cf, del.Key))
+		}
+	}
+	if err := txn.Commit(); err != nil {
+		return err
+	}
+	defer txn.Discard()
 	return nil
 }
